c6e7379f3e3069fa6abda98aa05895e65bc1d884
Zach Smith <zachsmith.sa@gmail.com>
2018-08-03 14:13:10 +0200

    adjusted text - csv module to use async csv parser

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const jsonfile = require('jsonfile');
 const TaskManager = require('./lib/task-manager');
 const Tools = require('./lib/tools');
 const log = Tools.log;
-const CLI = Tools.CLI;
 const path = require('path').posix;
 path.sep = "/";
 __dirname = __dirname.split(/\\/g).join('/');
@@ -51,8 +50,121 @@ packageOptions.functions
         netl.loadFunctionModule(require(filePath));
     });
 
-module.exports = (() => {
+/*
+ ******************************************
+ ******************************************
+ ******************************************
+ ****************** CLI *******************
+ ******************************************
+ ******************************************
+ ******************************************
+ */
+const CLI = (function() {
+    var rl;
 
+    /**
+     * Loads a JSON file from taskPath
+     * Executes tasks from JSON configuration
+     * @param {string} taskPath 
+     */
+    function loadTask(taskPath) {
+        // Read Task configuration
+        try {
+            var configFile = jsonfile.readFileSync(taskPath);
+        } catch (error) {
+            log.error("Error loading task: reading config file:\n" + error);
+            log.console("Error loading task: reading config file:\n" + error);
+            return;
+        };
+        // Load all tasks in the config file
+        configFile.forEach(async(configuration, i, arr) => {
+            // Create task
+            log.info(`Creating new task : ${configuration.ID}`);
+            log.console(`Creating new task : ${configuration.ID} (logging to ${log.logPath})`);
+            // Wait for task completion
+            const taskResult = await netl.taskManager.doTask(configuration);
+            log.info(taskResult);
+            log.console(taskResult);
+            // Delete task
+            var killResult
+            try {
+                killResult = await netl.taskManager.killTask(configuration.ID);
+            } catch (error) {
+                killResult = "Error killing task: " + error;
+            };
+            log.info(killResult);
+            log.console(killResult);
+        });
+    };
+
+    /**
+     * Processes user input
+     * @param {string} input 
+     */
+    function handleInput(input) {
+        process.stdout.write("> ");
+        var inputs = input.split(' ');
+        var cmd = inputs[0].toUpperCase();
+        var cmd2;
+        var fPath;
+        switch (cmd) {
+            case 'LOAD':
+                cmd2 = (inputs[1]) ? inputs[1].toUpperCase() : null;
+                switch (cmd2) {
+                    case 'FILE':
+                        fPath = (inputs[2]) ? path.normalize(inputs[2]) : null;
+                        if (fPath) {
+                            loadTask(fPath);
+                            break;
+                        };
+                    default:
+                        log.console("Incorrect argument for 'LOAD': 'load file <path>'");
+                        break;
+                };
+                break;
+            case 'SETLOG':
+                fPath = (inputs[1]) ? inputs[1] : null;
+                if (fPath) {
+                    log.setLogPath(fPath);
+                    break;
+                }
+                break
+            default:
+                log.console();
+                log.console("Unknown CMD. Available commands are:");
+                log.console('1. load file <file>');
+                log.console('2. setlog <file>');
+        };
+    };
+
+    /**
+     * Starts CLI
+     */
+    function start() {
+        const readline = require('readline');
+        rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        }).on('line', (input) => {
+            handleInput(input);
+        });
+    };
+
+    /**
+     * Stops the CLI
+     */
+    function stop() {
+        rl.close();
+    };
+
+    return {
+        start: start,
+        stop: stop,
+        loadTask: loadTask
+    };
+})();
+
+module.exports = (() => {
     // Log start of app
     log.info(`*** Started ${npmConfig.name} V${npmConfig.version} © ${npmConfig.copyright}`);
     log.info(`Timestamps in UTC time`);
